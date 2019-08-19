ed88ca94aef88adcb1324c0ce7e59dbdb5da6bff
takehito kondo <kondo@quark.hiroshima-u.ac.jp>
2019-08-19 22:30:06 +0900

    [fix] fix dotfilesLink.sh about vim files
    fix the file fit for the moving their files into .vim

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-ln -sf ~/gitDotFiles/vimrc ~/.vim/vimrc
-ln -sf ~/gitDotFiles/root.vim ~/.vim/syntax/root.vim
+ln -sf ~/gitDotFiles/.vim/vimrc ~/.vim/vimrc
+ln -sf ~/gitDotFiles/.vim/syntax/root.vim ~/.vim/syntax/root.vim
 ln -sf ~/gitDotFiles/.emacs.d/init.el ~/.emacs.d/init.el
 ln -sf ~/gitDotFiles/.emacs.d/Cask ~/.emacs.d/Cask
 ln -sf ~/gitDotFiles/.zshrc ~/.zshrc
