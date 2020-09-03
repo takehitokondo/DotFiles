f7cbefbad00fe6412f61cdedc9dab758d94cac93
Takehito Kondo <kondomac2020@takondo-air-2020.local>
2020-09-03 10:52:11 +0900

    Add one line for mkdir

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-ln -sf ~/DotFiles/.vim/vimrc ~/.vim/vimrc
-ln -sf ~/DotFiles/.vim/syntax/root.vim ~/.vim/syntax/root.vim
-ln -sf ~/DotFiles/.emacs.d/init.el ~/.emacs.d/init.el
-ln -sf ~/DotFiles/.emacs.d/Cask ~/.emacs.d/Cask
-ln -sf ~/DotFiles/.zshrc ~/.zshrc
-ln -sf ~/DotFiles/.rootlogon.C ~/.rootlogon.C
+mkdir -p ~/.vim/ ~/.vim/syntax ~/.emacs.d
+ln -sf ~/.DotFiles/.vim/vimrc ~/.vim/vimrc
+ln -sf ~/.DotFiles/.vim/syntax/root.vim ~/.vim/syntax/root.vim
+ln -sf ~/.DotFiles/.emacs.d/init.el ~/.emacs.d/init.el
+ln -sf ~/.DotFiles/.emacs.d/Cask ~/.emacs.d/Cask
+ln -sf ~/.DotFiles/.zshrc ~/.zshrc
+ln -sf ~/.DotFiles/.rootlogon.C ~/.rootlogon.C
